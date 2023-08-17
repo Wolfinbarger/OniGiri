607c8c306f1eeedb3dbb65a4051d6ce28322c2bd
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-08-16 21:42:07 -0500

    basic migration file to add additional fields to user

---
+++ db/migrate/20230817023027_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :user_address, :string
+  end
+end
