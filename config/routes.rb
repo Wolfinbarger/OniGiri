76d768dc00764a788243dcd44b0ff1fdbf861c91
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-08-20 15:16:27 -0500

    configure registration with new custom fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   # Defines the root path route ("/")
   # See app/javascript/components/Home.jsx
